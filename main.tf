4462d4e163922cdaa9665406e8283f585c5258bd
Hamed Momeni <537522+2hamed@users.noreply.github.com>
2025-05-29 10:12:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,13 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.0.0"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
+locals {
+  project_name = "dummy-project"
+  environment  = "development"
+  owner        = "hamed"
   tags = {
-    Name = var.instance_name
+    team    = "sre"
+    purpose = "testing"
   }
 }
